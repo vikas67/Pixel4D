b329f3c0de55d84e765df71617243983a1753912
ArjunAndroid <arjundeveloper97@gmail.com>
2021-03-01 15:04:02 +0530

    Initial commit

---
+++ app/src/main/java/com/apptech/pixel4d/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.apptech.pixel4d.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
